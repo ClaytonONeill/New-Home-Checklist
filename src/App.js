f7f98991d34cfbdd19d8218b8f09ed011a36f0bb
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-06-28 11:11:52 -0400

    started on the foundation of the appliation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    view: {
+      page: 'home',
+      pageTitle: 'Home Page'
+    }
+  }
+
+
+  handleView = (view) =>  {
+
+  }
+
+
+  render () {
+    return (
+      <div>
+        <h1>This is the home page</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
